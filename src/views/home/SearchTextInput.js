be46b0c1ff5b6dc8d0597d560ba5afff8afc73c8
rezahs95 <rezahs1995@gmail.com>
2018-09-23 01:01:52 +0330

    Search Input updated

--- src/views/home/SearchTextInput.js
+++ src/views/home/SearchTextInput.js
@@ -6,13 +6,38 @@ import styles from 'src/consts/styles'
 type searchTextInputProps = {
   placeholder: string,
 }
-const SearchTextInput = (props: searchTextInputProps) => {
-  const {placeholder} = props
-  const {home} = styles
-  return (
-      <div className='input-wrapper'>
-        {/*language=SCSS*/}
-        <style jsx>{`
+
+class SearchTextInput extends React.Component <searchTextInputProps> {
+  constructor(props) {
+    super(props)
+    this.state = {filled: false, emptied: false}
+  }
+
+  textChange = (event) => {
+
+    if (event.target.value === '') {
+      if(this.state.filled){
+        this.setState({...this.state, emptied: true, filled: false})
+      }
+      else{
+        this.setState({...this.state, emptied: false, filled: false})
+      }
+    } else {
+      this.setState({...this.state, filled: true, emptied: false})
+    }
+  }
+
+  focusOut = (event) => {
+    this.setState({...this.state, emptied: fals e})
+  }
+  render() {
+    const {placeholder} = this.props
+    const {home} = styles
+
+    return (
+        <div className='input-wrapper'>
+          {/*language=SCSS*/}
+          <style jsx>{`
           .input-wrapper {
           }
 
@@ -45,6 +70,23 @@ const SearchTextInput = (props: searchTextInputProps) => {
             font-size: 180%;
           }
 
+          .input-field2 {
+            position: relative;
+            display: block;
+            float: right;
+            padding: 0.8em;
+            border: none;
+            border-radius: 0;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            width: 100%;
+            background: transparent;
+            color: ${home.color.searchInputBorder};
+            text-align: center;
+            transition: opacity 0.3s;
+            font-weight: bold;
+            font-size: 180%;
+          }
+
           .input-label {
             display: inline-block;
             float: right;
@@ -59,12 +101,53 @@ const SearchTextInput = (props: searchTextInputProps) => {
             pointer-events: none;
           }
 
+          .input-label2 {
+            display: inline-block;
+            float: right;
+            padding: 0 1em;
+            font-weight: bold;
+            font-size: 100%;
+            user-select: none;
+            position: absolute;
+            left: 0;
+            width: 100%;
+            color: ${home.color.searchInputLabel};
+            pointer-events: none;
+          }
+
+          .input-label2::before{
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 50%;
+            height: 100%;
+            border: 4px solid ${home.color.searchInputLabelBorder};
+            transition: transform 0.3s;
+            border-right: none;
+            transform: translate3d(-10%, 0, 0);
+            }
+
+          .input-label2::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            width: 50%;
+            height: 100%;
+            border: 4px solid ${home.color.searchInputLabelBorder};
+            transition: transform 0.3s;
+            left: 50%;
+            border-left: none;
+            transform: translate3d(10%, 0, 0);
+            }
+
           .input-label-content {
             position: relative;
             display: block;
             padding: 1.6em 0;
             width: 100%;
             text-align: center;
+            top: ${this.state.filled ? '4.5em' : (this.state.emptied ? '4.5em': '0')};
           }
 
           .input-label::before,
@@ -107,29 +190,31 @@ const SearchTextInput = (props: searchTextInputProps) => {
 
           @keyframes anim-2 {
             50% {
-              opacity: 0;
-              transform: scale3d(0.3, 0.3, 1);
+              //opacity: 0;
+              //transform: scale3d(0.3, 0.3, 1);
             }
             51% {
-              opacity: 0;
-              transform: translate3d(0, 4.5em, 0) scale3d(0.3, 0.3, 1);
+              //opacity: 0;
+              //transform: translate3d(0, 4.5em, 0) scale3d(0.3, 0.3, 1);
             }
             100% {
               opacity: 1;
-              transform: translate3d(0, 4.5em, 0);
+              transform: ${!this.state.emptied && 'translate3d(0, 4.5em, 0)'} ;
             }
           }
         `}</style>
 
-        <span className="input">
-					<input className="input-field" type="text" id="input-7"/>
-					<label className="input-label" htmlFor="input-7">
-						<span className="input-label-content">{placeholder}</span>
+          <span className="input">
+					<input className={!this.state.filled ? "input-field" : "input-field2"} type="text" id="input-7"
+                 onChange={this.textChange} onBlur={this.focusOut}/>
+					<label className={!this.state.filled ? "input-label" : "input-label2"} htmlFor="input-7">
+						<span
+                className="input-label-content">{placeholder}</span>
 					</label>
 				</span>
-      </div>
-  )
-
+        </div>
+    )
+  }
 }
 
 SearchTextInput.propTypes = {
